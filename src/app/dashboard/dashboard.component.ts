abfd3c0b442e2145c4bc5647d645306a1e24a9c0
AishwaryaMittal <43309452+AishwaryaMittal@users.noreply.github.com>
2018-10-24 23:11:01 -0500

    Add files via upload

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -8,6 +8,9 @@ import { DashboardService } from './dashboard.service';
 })
 export class DashboardComponent implements OnInit {
   searches: any[];
+  firstNamesRef: any[];
+  lastNamesRef: any[];
+  fullNamesRef: any[];
   constructor(private dashboardService: DashboardService) {
     this.searches = [];
   }
@@ -20,5 +23,33 @@ export class DashboardComponent implements OnInit {
 
   ngOnInit() {
   }
+  getData(){
+    
+    this.dashboardService.getFirstNames().subscribe((firstNames: any) =>{
+    this.firstNamesRef=firstNames;
+      console.log(" this is the firstNames");
+      //console.log(firstNames);
+      console.log(" this is the firstNames");
+    }
+    );
+  }
+
+  getLastNameData(){
+    
+    this.dashboardService.getLastNames().subscribe((lastNames: any) =>{
+    this.lastNamesRef=lastNames;
+      console.log(" this is the lastNames");
+      //console.log(lastNames);
+      console.log(" this is the lastNames");
+    }
+    );
+    }
+
+
+  getFullNameData(){
+    
+    console.log(" this is the Full Names");
+    console.log(this.getData()+this.getLastNameData());
+    }
 
 }
--- src/app/dashboard/dashboard.service.ts
+++ src/app/dashboard/dashboard.service.ts
@@ -5,14 +5,26 @@ import { AngularFireDatabase } from '@angular/fire/database';
 @Injectable()
 export class DashboardService {
   searchHistoryRef: any;
+  firstNamesRef: any;
+  lastNamesRef: any;
   constructor(
     private loginService: LoginService,
     private db: AngularFireDatabase,
+
     ) {
     this.searchHistoryRef = this.db.list(`currentSession/${this.loginService.userUid}/searches`);
+    //this.searchHistoryRef.push({ firstName: firstName, lastName: lastName});
+    this.firstNamesRef= this.db.list('firstNames');
+    this.lastNamesRef= this.db.list('lastNames');
   }
 
   getSearchHistory() {
     return this.searchHistoryRef.valueChanges();
   }
-}
+  getFirstNames(){
+    return this.firstNamesRef.valueChanges(); 
+  }
+  getLastNames(){
+    console.log("in last names");
+   return this.lastNamesRef.valueChanges();  
+  }
